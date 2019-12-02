450efa26550ad7e1e375d24e4cf971cddf3fae62
Swathi-anand707 <swathi.anand97@gmail.com>
2019-12-02 23:42:59 +0530

    Spring-test(dec-02-19) commit

--- Spring test/productapp/src/main/java/com/ustglobal/productapp/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.ustglobal.productapp.dao;
-
-import com.ustglobal.productapp.beans.Product;
-
-public interface ProductDao {
-	 	public Product  searchProduct(int pid);
-
-}
--- Spring test/productapp/src/main/java/com/ustglobal/productapp/repo/ProductService.java
+++
@@ -1,8 +0,0 @@
-package com.ustglobal.productapp.repo;
-
-import com.ustglobal.productapp.beans.Product;
-
-public interface ProductService {
-	public Product  searchProduct(int pid);
-
-}
